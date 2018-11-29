639b8d4c3e3057d4622a9b26acb28a5b0ba926b0
Adriantorres <adrianalbertotorres@gmail.com>
2018-11-29 16:47:33 -0300

    rutas dinamicas

---
+++ meu/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'inicio', component:InicioComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
